6ac6e27ed3f4f49fee067982152d56aed44af2db
AhmadMorshed <ahmadnakawa676@gmail.com>
2024-09-28 09:36:18 -0700

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Repository.Specification.ProductSpecs;
+using Store.Services.Services.ProductServices.Dto;
+
+namespace Store.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<BrandTypeDetailsDto>>> GetAllBrands()
+            => Ok(await _productService.GetAllBrandsAsyns());
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<BrandTypeDetailsDto>>> GetAllTypes()
+            => Ok(await _productService.GetAllTypesAsync());
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ProductDetailsDto>>> GetAllProducts(ProductSpecification input)
+            => Ok(await _productService.GetAllProductAsync(input));
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<BrandTypeDetailsDto>>> GetProductById(int? id)
+            => Ok(await _productService.GetProductByIdsAsync(id));
+
+
+
+
+    }
+}
